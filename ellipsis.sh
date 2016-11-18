0f6ef8d1ae87cf0658a6648d2a8b60df62d3071f
Torgny Bjers <torgny@clear-markets.com>
2016-11-18 01:58:23 -0500

    Installing prerequisites

--- ellipsis.sh
+++ ellipsis.sh
@@ -5,6 +5,9 @@
 pkg.install() {
   case $(os.platform) in
     linux)
+      if utils.cmd_exists pacman; then
+        sudo pacman -Sy --noconfirm gconf
+      fi
       utils.run_installer https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh
       ;;
   esac
